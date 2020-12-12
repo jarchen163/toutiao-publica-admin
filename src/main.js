3bf05723d9e5e3dcff42f4887ac9af3efcf23f0d
chenyulong <jar_chen@163.com>
2020-12-12 20:25:47 +0800

    '调整初始目录结构'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+/**
+ * 项目启动口
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入全局样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+// 创建vue根实例
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app') // 挂载index页面中 id是app的
